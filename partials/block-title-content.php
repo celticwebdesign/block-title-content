bb8d9b657a0224682c840aa42e48b2c8736646c9
Darren Stevens <darren.stevens@celticwebdesign.net>
2019-06-09 11:14:32 +0100

    Added ACF field group Content.

--- partials/block-title-content.php
+++ partials/block-title-content.php
@@ -1,18 +1,40 @@
 <?php
 
-    if ( get_field('block_title_content__title_g') ) {
+    if (
+        get_field('block_title_content__title_g') ||
+        get_field('block_title_content__content_g')
+    ) {
 
-        $title_content  = get_field('block_title_content__title_g');
-        $type           = $title_content['block_title_content__title_g__type'];
-        $title          = $title_content['block_title_content__title_g__title'];
-        $alignment      = $title_content['block_title_content__title_g__alignment'];
-        $underline      = $title_content['block_title_content__title_g__underline'];
+        echo '<section class="block-title-content">';
 
-        echo '<div class="block-title-content">
-                <'.$type.' class="'.$alignment.' '.$underline.'">
-                    '.$title.'
-                </'.$type.'>
-            </div>';
+            if ( get_field('block_title_content__title_g') ) {
+
+                $title          = get_field('block_title_content__title_g');
+                $type           = $title['block_title_content__title_g__type'];
+                $title          = $title['block_title_content__title_g__title'];
+                $alignment      = $title['block_title_content__title_g__alignment'];
+                $underline      = $title['block_title_content__title_g__underline'];
+
+                echo '<div class="block-title-content__title">
+                        <'.$type.' class="'.$alignment.' '.$underline.'">
+                            '.$title.'
+                        </'.$type.'>
+                    </div>';
+
+            }
+
+            if ( get_field('block_title_content__content_g') ) {
+
+                $content        = get_field('block_title_content__content_g');
+                $text           = $content['block_title_content__content_g__text'];
+
+                echo '<div class="block-title-content__content">
+                        '.$text.'
+                    </div>';
+
+            }
+
+        echo '</section>'; // .block-title-content
 
     }
 
