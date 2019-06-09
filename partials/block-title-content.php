75d6049ebed1bbc9b1dc349d5af090739dd5b421
Darren Stevens <darren.stevens@celticwebdesign.net>
2019-06-09 11:32:55 +0100

    Bugfix to stop illegal string offset.

--- partials/block-title-content.php
+++ partials/block-title-content.php
@@ -9,11 +9,11 @@
 
             if ( get_field('block_title_content__title_g') ) {
 
-                $title          = get_field('block_title_content__title_g');
-                $type           = $title['block_title_content__title_g__type'];
-                $title          = $title['block_title_content__title_g__title'];
-                $alignment      = $title['block_title_content__title_g__alignment'];
-                $underline      = $title['block_title_content__title_g__underline'];
+                $title_g        = get_field('block_title_content__title_g');
+                $type           = $title_g['block_title_content__title_g__type'];
+                $title          = $title_g['block_title_content__title_g__title'];
+                $alignment      = $title_g['block_title_content__title_g__alignment'];
+                $underline      = $title_g['block_title_content__title_g__underline'];
 
                 echo '<div class="block-title-content__title">
                         <'.$type.' class="'.$alignment.' '.$underline.'">
@@ -25,8 +25,8 @@
 
             if ( get_field('block_title_content__content_g') ) {
 
-                $content        = get_field('block_title_content__content_g');
-                $text           = $content['block_title_content__content_g__text'];
+                $content_g      = get_field('block_title_content__content_g');
+                $text           = $content_g['block_title_content__content_g__text'];
 
                 echo '<div class="block-title-content__content">
                         '.$text.'
